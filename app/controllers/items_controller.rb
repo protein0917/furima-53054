e13482808c4029152ac0b5eb78e33f463632f7e7
protein0917 <80035163+protein0917@users.noreply.github.com>
2021-04-09 13:16:05 +0900

    items_controller.rb コメントアウト解除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,17 @@
 
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: :index
+  before_action :move_to_index, except: :index
 
   def index
     @items = Item.all
   end
 
 
-  # private
+  private
 
-  # def move_to_index
-    # unless user_signed_in?
-      # redirect_to action: :index
-    # end
-  # end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
