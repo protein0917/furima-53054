52b7ed1bf7a2cbfd2a048b94440858442dc1706b
protein0917 <80035163+protein0917@users.noreply.github.com>
2021-04-11 18:37:31 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,30 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 
   private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :condition_id, :shipping_free_id, :shipping_area_id, :shipping_day_id, :category_id, :image).merge(user_id: current_user.id)
+  end
 
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
   end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,27 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :image
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :shipping_free_id, numericality: { other_than: 1 }
+    validates :shipping_area_id, numericality: { other_than: 1 }
+    validates :shipping_day_id, numericality: { other_than: 1 }
+    validates :category_id, numericality: { other_than: 1 }
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_free
+  belongs_to :shipping_area
+  belongs_to :shipping_day
+
+  has_one_attached :image
+
+
 end
